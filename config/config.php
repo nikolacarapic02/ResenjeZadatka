2f94a314256fec4f8038a48513e9524ca3f50b32
nikolacarapic02 <nikola.carapic@quantoxtechnology.com>
2021-09-30 21:25:48 +0200

    Ažuriran fajl config.php

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
 
-define("HOST","localhost");
-define("USERNAME", "Nikola");
-define("PASSWORD", "Nikola9122002");
-define("DBNAME","QuantoxPraksa");
+define("HOST","");
+define("USERNAME", "");
+define("PASSWORD", "");
+define("DBNAME","");
